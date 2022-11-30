ca1475210f6d18c12d8f144917350481cfd7df1e
Fauthe <manishlama731@gmail.com>
2022-11-30 08:08:15 +0545

    First C# console app added

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+string name = ".NET Training";
+
+Console.WriteLine(name);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Manish is God!!!");
+Console.WriteLine("Worship Manish!!!");
