a9f91feca35d1e7f75337ac3481d2df341b97945
Fauthe <manishlama731@gmail.com>
2022-12-05 21:49:28 +0545

    New Branch created Evrything integrated MenuSystem

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,12 +1,44 @@
-﻿class EntryPoint
+﻿using System;
+class EntryPoint
 {
     public static void Main()
     {
-        Selection s = new();
-        s.LearnSelectionStatements();
 
-        Loops l = new();
-        l.LearnLoops();
+        menu m = new();
+        m.main_menu();
+        byte ch = byte.Parse(Console.ReadLine());
+
+        while (ch != 0)
+        {
+            switch (ch)
+            {
+                case 1:
+                    Loops lp = new();
+                    m.loop_menu();
+                    lp.looping_body();
+                    break;
+                case 2:
+                    Selection sel = new();
+                    m.SelectionStatement_Menu();
+                    sel.SelectionStatementsBody();
+                    break;
+                case 3:
+                    assign1 a1 = new();
+                    a1.AssignObj1();
+                    a1.AssignObj2();
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input...\nPlease provide valid input!!!");
+                    break;
+
+
+            }
+            m.main_menu();
+            ch = byte.Parse(Console.ReadLine());
+        }
+
 
     }
 }
