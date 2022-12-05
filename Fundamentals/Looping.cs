d7b32d9c8d5d50f3e8f97ab243c53c002c8f55b2
Fauthe <manishlama731@gmail.com>
2022-12-05 08:02:09 +0545

    Learned about loops and refactoring

---
+++ Fundamentals/Looping.cs
@@ -0,0 +1,51 @@
+class Loops
+{
+    public void LearnLoops()
+    {
+        // for (int i = 1; i < 11; i++)
+        // {
+        //     Console.WriteLine("Nepal");
+        // }
+
+        // for (byte i = 1; i < 101; i++)
+        // {
+        //     Console.Write(i + " ");
+        // }
+
+        // CW - Print even numbers less than 500
+        //Console.WriteLine("Even Numbers less than 500 are as follows:");
+        // for (int num = 0; num < 500; num++)
+        // {
+        //     if (num % 2 == 0)
+        //     {
+        //         Console.Write(num + " ");
+        //     }
+        // }
+
+
+        // CW - Print all number less than 500 which are multiple of 3 and 5
+        //Console.WriteLine("Numbers divisible by 3 and 5 less than 500 are as follows: ");
+        // for (int i = 0; i < 500; i++)
+        // {
+        //     if (i % 3 == 0 && i % 5 == 0)
+        //     {
+        //         Console.Write(i + " ");
+        //     }
+        // }
+
+        // byte n = 1;
+        // while (n <= 100)
+        // {
+        //     Console.Write(n + " ");
+        //     n++;
+        // }
+
+        // While - use case
+        string key = "y";
+        while (key == "y")
+        {
+            Console.WriteLine("Nepal");
+            key = Console.ReadLine();
+        }
+    }
+}
--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -2,75 +2,11 @@
 {
     public static void Main()
     {
-        //Console.WriteLine("Nepal is beautiful country");
-        // Console.Write("Enter Your Name: ");
-        // string x = Console.ReadLine();
-        // Console.WriteLine("Your Name is: " + x);
+        Selection s = new();
+        s.LearnSelectionStatements();
 
-        //Conditionals, Selection statements
-        // If-else, if-elseif-else, switch
-        // Branching
+        Loops l = new();
+        l.LearnLoops();
 
-        // short age = 23;
-        // Console.Write("Enter Your Age: ");
-        // short age = Convert.ToInt16(Console.ReadLine());
-        // if (age > 18) // comparison operators >, <, <=, >=, !=, ==, &&, ||
-        // {
-        //     Console.WriteLine("You are old. hahaha");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("You're still a kid. hahaha.");
-        // }
-
-        // Console.Write("Enter Your Age: ");
-        // short age = Convert.ToInt16(Console.ReadLine());
-        // if (age < 18) // comparison operators >, <, <=, >=, !=, ==, &&, ||
-        // {
-        //     Console.WriteLine("You are child.");
-        // }
-        // else if (age < 40)
-        // {
-        //     Console.WriteLine("You're adult.");
-        // }
-        // else if (age < 60)
-        // {
-        //     Console.WriteLine("You are in your middle age.");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("You are old. Hahahaha");
-        // }
-
-        Console.Write("Enter your rashi: ");
-        string rashi = Console.ReadLine();
-        if (rashi == "Mesh")
-        {
-            Console.WriteLine("Mesh rashi initials are ka, kha...");
-        }
-        else if (rashi == "Tula")
-        {
-            Console.WriteLine("Tula rashi initials are Ga, Gha...");
-        }
-        else
-        {
-            Console.WriteLine("Brisha rashi initials are Ba, Bi...");
-        }
-
-        switch (rashi)
-        {
-            case "Mesh":
-                Console.WriteLine("Mesh rashi initials are ka, kha...");
-                break;
-            case "Tula":
-                Console.WriteLine("Tula rashi initials are Ga, Gha...");
-                break;
-            case "Brisha":
-                ("Brisha rashi initials are Ba, Bi...");
-                break;
-            default:
-                Console.WriteLine("Unknown rashi");
-                break;
-        }
     }
 }
